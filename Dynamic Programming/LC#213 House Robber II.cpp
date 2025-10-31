c6595fb91d8e9046b86764991bae849dbc0e37d3
Abhishek <90861021+Abhishek220801@users.noreply.github.com>
2025-10-31 16:14:32 +0530

    Update LC#213 House Robber II.cpp

--- Dynamic Programming/LC#213 House Robber II.cpp
+++ Dynamic Programming/LC#213 House Robber II.cpp
@@ -56,3 +56,33 @@ public:
         return max(res1, res2);
     }
 };
+
+
+// Space Optimization 
+class Solution {
+public:
+    int solve(vector<int>& nums, int l, int r){
+        int prevPrev = 0, prev = 0, ans;
+
+        for(int i=l; i<=r; i++){
+            int skip = prev;
+            int take = nums[i]+ prevPrev;
+            ans = max(skip, take);
+            prevPrev = prev;
+            prev = ans;
+        }
+        
+        return prev;
+    }
+
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1) return nums[0];
+        if(n==2) return max(nums[0], nums[1]);
+
+        int take_0th = solve(nums, 0, n-2);
+        int skip_0th = solve(nums, 1, n-1);
+
+        return max(take_0th, skip_0th);
+    }
+};
