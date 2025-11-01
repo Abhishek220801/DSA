7b88cf68f0a13fa32c0f9c57d988d8bde3e320c6
Abhishek <90861021+Abhishek220801@users.noreply.github.com>
2025-11-01 15:15:48 +0530

    Update LC#389 Find the Difference .cpp

--- Character Arrays & Strings/LC#389 Find the Difference .cpp
+++ Character Arrays & Strings/LC#389 Find the Difference .cpp
@@ -31,3 +31,21 @@ public:
         return (char)(sum_t - sum_s);
     }
 };
+
+// Approach 3: using XOR operator
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int XOR = 0;
+
+        for(char &ch: s){
+            XOR ^= ch;
+        }
+
+        for(char &ch: t){
+            XOR ^= ch;
+        }
+
+        return (char)XOR;
+    }
+};
