e2bbdc57dcd8680e20da81b7999ea2d8e00c8cce
Abhishek <90861021+Abhishek220801@users.noreply.github.com>
2025-11-01 11:46:05 +0530

    Update LC#1911 Maximum Alternating Subsequence Sum.cpp

--- Dynamic Programming/LC#1911 Maximum Alternating Subsequence Sum.cpp
+++ Dynamic Programming/LC#1911 Maximum Alternating Subsequence Sum.cpp
@@ -45,3 +45,22 @@ public:
         return solve(nums, 0, true);  // 0 - even index, true : + 
     }
 };
+
+
+// Bottom Up 
+class Solution {
+public:
+    long long maxAlternatingSum(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<long>> t(n+1, vector<long>(2, 0)); // even : 0 , odd : 1
+
+        for(int i=1; i<=n; i++){
+            // Even
+            t[i][0] = max(t[i-1][1] - nums[i-1], t[i-1][0]);
+            // Odd
+            t[i][1] = max(t[i-1][0] + nums[i-1], t[i-1][1]);
+        }
+
+        return max(t[n][0], t[n][1]);
+    }
+};
