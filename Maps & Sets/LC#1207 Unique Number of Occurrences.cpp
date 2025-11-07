f687a47a723a2b34594380e26412876ea05bb359
Abhishek <90861021+Abhishek220801@users.noreply.github.com>
2025-11-07 09:27:00 +0530

    Create LC#1207 Unique Number of Occurrences.cpp

---
+++ Maps & Sets/LC#1207 Unique Number of Occurrences.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/unique-number-of-occurrences
+
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int,int> mp;
+        for(auto &x: arr){
+            mp[x]++;
+        }
+        unordered_set<int> s;
+        for(auto &x: mp){
+            if(s.find(x.second)!=s.end()) return false;
+            s.insert(x.second);
+        }
+        return true;
+    }
+};
