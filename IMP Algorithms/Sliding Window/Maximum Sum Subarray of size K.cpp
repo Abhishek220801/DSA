13a26aabce373f8377b452a25af825f41fe72d53
Abhishek <90861021+Abhishek220801@users.noreply.github.com>
2026-03-18 09:54:45 +0530

    Update Maximum Sum Subarray of size K.cpp

--- IMP Algorithms/Sliding Window/Maximum Sum Subarray of size K.cpp
+++ IMP Algorithms/Sliding Window/Maximum Sum Subarray of size K.cpp
@@ -1,3 +1,5 @@
+// https://www.geeksforgeeks.org/problems/max-sum-subarray-of-size-k5313/1
+
 // #1. Brute Force 
 #include <bits/stdc++.h>
 using namespace std;
@@ -21,33 +23,22 @@ int main() {
 }
 
 // 2. Sliding Window Approach 
-// Online C++ compiler to run C++ program online
-#include <iostream>
-#include <limits.h>
-using namespace std;
-
-int main() {
-    int arr[] = {7,1,2,5,8,4,9,3,6};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    
-    int k = 3;
-    int maxSum = INT_MIN;
-    int sum;  
-    int prevSum = 0, currSum = 0;
-    for(int i=0; i<k; i++){
-        prevSum+=arr[i];
+class Solution {
+  public:
+    int maxSubarraySum(vector<int>& arr, int k) {
+        // code here
+        int prevSum = 0, currSum = 0, maxSum = INT_MIN;
+        for(int i=0; i<k; i++)
+            prevSum += arr[i];
+        maxSum = max(maxSum, prevSum);
+        int i = 1, j = i+k-1, n = arr.size();
+        while(j<n){
+            currSum = prevSum+arr[j]-arr[i-1];
+            maxSum = max(maxSum, currSum);
+            prevSum = currSum;
+            i++;
+            j++;
+        }
+        return maxSum;
     }
-    int i = 1, j = i+k-1;
-    maxSum = max(maxSum, prevSum);
-    while(j<n){
-        currSum = prevSum + arr[j] - arr[i-1];
-        maxSum = max(maxSum, currSum);
-        prevSum = currSum;
-        i++;
-        j++;
-    }
-    
-    cout<<"Maximum sum subarray of size "<<k<<" = "<<maxSum<<endl;
-
-    return 0;
-}
+};
