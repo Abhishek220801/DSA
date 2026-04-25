e9a9ed05abd29217df76d1f25515d39c630d1831
Abhishek <90861021+Abhishek220801@users.noreply.github.com>
2026-04-25 10:04:38 +0530

    Add problem link to Two Sum.cpp
    Added problem link for Two Sum solution.

---
+++ Maps & Sets/LC#1 Two Sum.cpp
@@ -0,0 +1,17 @@
+https://leetcode.com/problems/two-sum/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> mp;
+        int n = nums.size();
+
+        for(int i=0; i<n; i++){
+            int rem = target - nums[i];
+            if(mp.find(rem) != mp.end()) return {i, mp[rem]};
+            mp[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
